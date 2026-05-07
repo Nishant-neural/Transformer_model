187764d1fdf92bdc4e2664c147569428f715e5d9
Nishant-neural <nishantiiwari@gmail.com>
2026-05-07 11:05:54 +0530

    added backwar dfor softmax

--- softmax.py
+++ softmax.py
@@ -9,3 +9,15 @@ def softmax(x):
     exp_x = np.exp(x_shifted)
     
     return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
+
+def softmax_backward(d_out, softmax_out):
+   
+   #   d_out: gradient wrt softmax output
+   #  softmax_out: output of softmax
+   #  shape: (..., seq_len)
+   
+    temp = np.sum(d_out * softmax_out, axis=-1, keepdims=True)
+
+    d_x = softmax_out * (d_out - temp)
+
+    return d_x
