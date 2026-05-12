e7a628fdcf4e48602564df2ce9e02e92dd3277d2
Nishant-neural <nishantiiwari@gmail.com>
2026-05-12 11:10:20 +0530

    Refactor MultiHeadAttention and ScaledDotProductAttention classes for improved forward and backward methods

--- attention.py
+++ attention.py
@@ -2,52 +2,58 @@ import numpy as np
 from softmax import softmax_forward , softmax_backward
 
 class ScaledDotProductAttention:
-    def __init__ (self , embed_dim , d_k , d_v) :
+ 
+    # def forward(self, X_Q, X_K, X_V, mask=None):
 
+    #     # X_q =  batch , seq_len_q , embed_dim
+    #     # X_K = batch , seq_len_k , embed_dim
+    #     # X_V = BATCH , seq_len_k , embed_dim
+    #     self.X_Q = X_Q
+    #     self.X_K = X_K
+    #     self.X_V = X_V
+    #     self.mask = mask
 
-        self.WQ = np.random.randn(embed_dim, d_k) / np.sqrt(embed_dim)
+    #     self.K = X_K @ self.WK
+    #     self.V = X_V @ self.WV
+    #     self.Q = X_Q @ self.WQ
 
-        self.dWQ = np.zeros_like(self.WQ)
-       
+    #     batch, seq_len_q, d_k =  self.Q.shape
+    #     self.scores = self.Q @  np.swapaxes(self.K, -1, -2)
 
-        self.WK = np.random.randn(embed_dim , d_k)/ np.sqrt(embed_dim)
-        self.dWK = np.zeros_like(self.WK)
-       
+    #     self.scores = self.scores / np.sqrt(d_k)
 
-        self.WV =  np.random.randn(embed_dim, d_v) / np.sqrt(embed_dim)
-        self.dWV = np.zeros_like(self.WV)
-       
+    #     if mask is not None:
+    #        self.scores = np.where(mask, self.scores, -1e9)
 
+    #     self.weights =  softmax_forward(self.scores)
+    #     self.out = self.weights @ self.V
 
-    def forward(self, X_Q, X_K, X_V, mask=None):
+    #     # weights = batch , seq_lenq , seq_len_k
+    #     # Out = batch , seq_len_q , d_v
+    #     return self.out, self.weights
 
-        # X_q =  batch , seq_len_q , embed_dim
-        # X_K = batch , seq_len_k , embed_dim
-        # X_V = BATCH , seq_len_k , embed_dim
-        self.X_Q = X_Q
-        self.X_K = X_K
-        self.X_V = X_V
+    def forward(self , Q, K, V, mask=None):
+        self.Q = Q    # Q = B H T D
+        self.K = K
+        self.V = V
         self.mask = mask
-
-        self.K = X_K @ self.WK
-        self.V = X_V @ self.WV
-        self.Q = X_Q @ self.WQ
-
-        batch, seq_len_q, d_k =  self.Q.shape
-        self.scores = self.Q @  np.swapaxes(self.K, -1, -2)
-
-        self.scores = self.scores / np.sqrt(d_k)
+        self.d_k = Q.shape[-1]
+        
+        self.scores = Q @ np.swapaxes(K, -1, -2)
+        self.scores = self.scores / np.sqrt(self.d_k)
 
         if mask is not None:
-           self.scores = np.where(mask, self.scores, -1e9)
+            if mask.ndim == 3:
+                mask = mask[:, None, :, :]  # (B, 1, Tq, Tk)
+            self.scores = np.where(mask, self.scores, -1e9)
 
-        self.weights =  softmax_forward(self.scores)
-        self.out = self.weights @ self.V
+        self.weights = softmax_forward(self.scores)
+        self.out = self.weights @ V
 
-        # weights = batch , seq_lenq , seq_len_k
-        # Out = batch , seq_len_q , d_v
+     # weights = batch ,heads, seq_lenq , seq_len_k
+        # Out = batch ,heads , seq_len_q , d_v
+        
         return self.out, self.weights
-
 
     @staticmethod
     def causal_mask(seq_len_q, seq_len_k=None):
@@ -62,12 +68,52 @@ class ScaledDotProductAttention:
         return (tokens != pad_id)[:, None, :]
 
      
+    # def backward(self , d_out ):
+    #     batch, seq_len_q, d_k =  self.Q.shape
+
+    #     self.dV =  self.weights.transpose(0,2,1)  @ d_out
+
+    #     self.dweights = d_out @ self.V.transpose(0,2,1)
+
+    #     self.dscaled =  softmax_backward(self.dweights , self.weights)
+    #     if self.mask is not None:
+    #         self.dscaled = np.where(self.mask, self.dscaled, 0)
+
+    #    # weights = softmax(scores)
+    #    # scores = scores/ scaling factor
+    #     # scores = Q @K.T
+
+    #     self.dscores = self.dscaled / np.sqrt(d_k)
+    #     self.dQ = self.dscores @ self.K
+    #     self.dK = self.dscores.transpose(0,2,1) @ self.Q
+
+    #     self.dWQ = np.sum(
+    #     self.X_Q.transpose(0,2,1) @ self.dQ,
+    #     axis=0
+    #     )
+    #     self.dX_Q = self.dQ @ self.WQ.T
+
+    #     self.dWK = np.sum(
+    #     self.X_K.transpose(0,2,1) @ self.dK,
+    #     axis=0
+    #     )
+    #     self.dX_K = self.dK @ self.WK.T
+
+    #     self.dWV = np.sum(
+    #     self.X_V.transpose(0,2,1) @ self.dV,
+    #     axis=0
+    #     )
+    #     self.dX_V = self.dV @ self.WV.T
+
+    #     return self.dX_Q, self.dX_K, self.dX_V
+    
     def backward(self , d_out ):
-        batch, seq_len_q, d_k =  self.Q.shape
+        batch, head_num, seq_len_q, d_k =  self.Q.shape
 
-        self.dV =  self.weights.transpose(0,2,1)  @ d_out
 
-        self.dweights = d_out @ self.V.transpose(0,2,1)
+        self.dV =  self.weights.transpose(0,1,3,2)  @ d_out
+
+        self.dweights = d_out @ self.V.transpose(0,1,3,2)
 
         self.dscaled =  softmax_backward(self.dweights , self.weights)
         if self.mask is not None:
@@ -79,41 +125,7 @@ class ScaledDotProductAttention:
 
         self.dscores = self.dscaled / np.sqrt(d_k)
         self.dQ = self.dscores @ self.K
-        self.dK = self.dscores.transpose(0,2,1) @ self.Q
+        self.dK = self.dscores.transpose(0,1,3,2) @ self.Q
 
-        self.dWQ = np.sum(
-        self.X_Q.transpose(0,2,1) @ self.dQ,
-        axis=0
-        )
-        self.dX_Q = self.dQ @ self.WQ.T
-
-        self.dWK = np.sum(
-        self.X_K.transpose(0,2,1) @ self.dK,
-        axis=0
-        )
-        self.dX_K = self.dK @ self.WK.T
-
-        self.dWV = np.sum(
-        self.X_V.transpose(0,2,1) @ self.dV,
-        axis=0
-        )
-        self.dX_V = self.dV @ self.WV.T
-
-        return self.dX_Q, self.dX_K, self.dX_V
-
-
-    def scaled_dot_product_attention(Q, K, V, mask=None):
-        d_k = Q.shape[-1]
-
-        scores = Q @ np.swapaxes(K, -1, -2)
-        scores = scores / np.sqrt(d_k)
-
-        if mask is not None:
-            if mask.ndim == 3:
-                mask = mask[:, None, :, :]  # (B, 1, Tq, Tk)
-            scores = np.where(mask, scores, -1e9)
-
-        weights = softmax_forward(scores)
-        out = weights @ V
-
-        return out, weights
+        return self.dV , self.dQ , self.dK
+   
--- multihead.py
+++ multihead.py
@@ -17,14 +17,13 @@ class MultiHeadAttention :
         self.WV = np.random.randn(num_heads, embed_dim, self.d_v) / np.sqrt(embed_dim)
 
          # out = weights @v for one head batch ,seq_len , d_v 
-         #  out(concatenated) =  batch , seq len , concat_dim
+         #  out(concatenated) =  batch , seq len , embed-dim
 
-        concat_dim = num_heads * self.d_v
-        self.WO = np.random.randn(concat_dim, embed_dim) / np.sqrt(concat_dim)
+        self.WO = np.random.randn(embed_dim, embed_dim) / np.sqrt(embed_dim)
         self.dWO = np.zeros_like(self.WO)
 
     def forward(self, X_Q, X_K, X_V, mask=None):
-    # X_Q: (B, Tq, E)
+    # X_Q: (B, Tq, E) T = seq_len  E = embed_dim
     # X_K: (B, Tk, E)
     # X_V: (B, Tk, E)
 
@@ -32,18 +31,48 @@ class MultiHeadAttention :
         K = np.einsum("bte,hed->bhtd", X_K, self.WK)
         V = np.einsum("bte,hed->bhtd", X_V, self.WV)
 
-    # Q, K, V are now:
-    # (B, H, T, D)
-        att = ScaledDotProductAttention()
-        head_out, weights = att.scaled_dot_product_attention(Q, K, V, mask)
+    # Q, K, V are now:   (B, H, T, D)
+   
+        self.att = ScaledDotProductAttention()
+        self.head_out, self.weights = self.att.forward(Q, K, V, mask)
 
     # head_out: (B, H, Tq, d_v)
 
-        head_out = np.transpose(head_out, (0, 2, 1, 3))
+        self.head_out = np.transpose(self.head_out, (0, 2, 1, 3))
     # (B, Tq, H, d_v)
 
-        B, Tq, H, Dv = head_out.shape
-        concat = head_out.reshape(B, Tq, H * Dv)
+        B, Tq, H, Dv = self.head_out.shape
+        self.concat = self.head_out.reshape(B, Tq, H * Dv)
 
-        out = concat @ self.WO
-        return out, weights
+        self.out = self.concat @ self.WO
+        return self.out, self.weights
+
+    def backward(self , d_out):
+        # self.dWQ = np.sum(
+    #     self.X_Q.transpose(0,2,1) @ self.dQ,
+    #     axis=0
+    #     )
+    #     self.dX_Q = self.dQ @ self.WQ.T
+
+    #     self.dWK = np.sum(
+    #     self.X_K.transpose(0,2,1) @ self.dK,
+    #     axis=0
+    #     )
+    #     self.dX_K = self.dK @ self.WK.T
+
+    #     self.dWV = np.sum(
+    #     self.X_V.transpose(0,2,1) @ self.dV,
+    #     axis=0
+    #     )
+    #     self.dX_V = self.dV @ self.WV.T
+
+    #     return self.dX_Q, self.dX_K, self.dX_V
+
+        self.dWO = self.concat.T @ d_out
+        # concat = B Tq embed_dim
+        # WO = embed , embed
+        # out = B Tq embed_dim
+        self.dconcat = d_out @ self.WO
+
+
+        return
